61e86a88ebfa558fc3078947adff154d1e324cea
adityjha0 <aditya2005aps@gmail.com>
2024-01-21 03:16:26 +0530

    time and size

--- main.py
+++ main.py
@@ -11,86 +11,71 @@ x=int(sys.argv[2])
 
 today = date.today()
 last = today - relativedelta(years=x) + relativedelta(days=1)
-print(last)
-print(today)
 t = str(symbol)
 
+df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
+columns = ['DATE', 'OPEN', 'CLOSE', 'HIGH','LOW', 'LTP', 'VOLUME', 'VALUE', 'NO OF TRADES']
+df = df.loc[:,columns]
+sizes=[]
+times=[]
 
 file=t+".csv"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_csv(file)
 etime = time.time() 
-size = (os.path.getsize(file))
-print(file)
-print(size)
-print((etime-stime)*1000)
-
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
 
 file=t+".json"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_json(file)
 etime = time.time() 
-size = (os.path.getsize(file))
-print(file)
-print(size)
-print((etime-stime)*1000)
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
 
 file=t+".html"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_html(file)
 etime = time.time() 
-size = (os.path.getsize(file))
-print(file)
-print(size)
-print((etime-stime)*1000)
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
 
 file=t+".tex"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_latex(file)
-etime = time.time() 
-size = (os.path.getsize(file))
-print(file)
-print(size)
-print((etime-stime)*1000)
+etime = time.time()
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
 
 file=t+".txt"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_csv(file, sep='\t', index=False)
 etime = time.time() 
-size = (os.path.getsize(file))
-print(file)
-print(size)
-print((etime-stime)*1000)
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
 
 file=t+".parquet"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_parquet(file, compression=None)
 etime = time.time() 
-size = (os.path.getsize(file))
-print(size)
-print((etime-stime)*1000)
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
 
 file=t+".orc"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_orc(file)
-etime = time.time() 
-size = (os.path.getsize(file))
-print(size)
-print((etime-stime)*1000)
+etime = time.time()
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
 
 file=t+".feather"
 stime = time.time()
-df = stock_df(symbol="SBIN", from_date=last,to_date=today, series="EQ")
 df.to_feather(file)
 etime = time.time() 
-size = (os.path.getsize(file))
-print(size)
-print((etime-stime)*1000)
+sizes.append(os.path.getsize(file))
+times.append((etime-stime)*10000)
+
+
+print(sizes)
+print(times)
 
