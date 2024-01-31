77f78ed5ebb5a58a51b91775da678cf0c0ca124c
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-01-31 18:59:22 +0530

    Added a new route for graph, and added a few buttons, with re-search feature

--- UserInterface/website/views.py
+++ UserInterface/website/views.py
@@ -1,12 +1,26 @@
 # this file handels the frontend that is the home page
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from datetime import *
 from dateutil.relativedelta import relativedelta
 from jugaad_data.nse import stock_df
 import plotly.graph_objs as go
+import pandas as pd
 
 views = Blueprint('views', __name__)
+PATH = "./website/DATA/df.pkl"
+
+def generate_candlestick_chart(PATH, symbol):
+    df = pd.read_pickle(PATH)
+    candlestick_trace = go.Candlestick(x=df['DATE'],
+                                    open=df['OPEN'],
+                                    high=df['HIGH'],
+                                    low=df['LOW'],
+                                    close=df['CLOSE'])
+
+    layout = go.Layout(title="Candlestick Chart", xaxis=dict(title='Date'), yaxis=dict(title='Price'),height=600, xaxis_rangeslider_visible=False,dragmode='pan')
+    figure = go.Figure(data=[candlestick_trace], layout=layout)
+    return figure.to_html(full_html=False)
 
 @views.route('/', methods = ['GET', 'POST'])
 @login_required
@@ -30,24 +44,21 @@ def home():
             symbol = request.form.get('symbol', default=df)
 
             df = stock_df(symbol = symbol, from_date=last, to_date=today, series="EQ")
-        
-        def generate_candlestick_chart():
-            candlestick_trace = go.Candlestick(x=df['DATE'],
-                                            open=df['OPEN'],
-                                            high=df['HIGH'],
-                                            low=df['LOW'],
-                                            close=df['CLOSE'])
-
-            layout = go.Layout(title='Candlestick Chart', xaxis=dict(title='Date'), yaxis=dict(title='Price'),height=600, xaxis_rangeslider_visible=False,dragmode='pan')
-
-            figure = go.Figure(data=[candlestick_trace], layout=layout)
-            return figure.to_html(full_html=False)
-    
-        if not df.empty:
-            candlestick_chart = generate_candlestick_chart()
-
-            print("yes")
-
-            return render_template("graph.html", user = current_user, candlestick_chart = candlestick_chart)
+            df = pd.to_pickle(df, PATH)
+            return redirect(url_for('views.graph', arg1 = PATH, arg2 =  symbol))
 
     return render_template("home.html", user = current_user)
+
+@views.route('/graph', methods = ['GET', 'POST'])
+@login_required
+def graph():
+    df = request.args.get('arg1')
+    symbol = request.args.get('arg2')
+    symbols = []
+    symbols.append(symbol)
+    if df:
+        candlestick_chart = generate_candlestick_chart(df, symbol)
+
+        return render_template("graph.html", user = current_user, candlestick_chart = candlestick_chart, symbols = symbols)
+
+    return redirect(url_for('views.home'))
