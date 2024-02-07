d8466df5fbbff47ba173b8962071651e8274b29b
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-02-07 17:05:51 +0530

    main.py ran through main.cpp

--- Stratergies/main.cpp
+++ Stratergies/main.cpp
@@ -49,6 +49,13 @@ int main(int argv, char *argc[])
         return 0;
     }
 
+    // --------------- generate data --------------------------
+    string symbol = argc[2];
+    string start_date = argc[5];
+    string end_date = argc[6];
+    string command0 = "python3 main.py " + symbol + " " + start_date + " " + end_date;
+    system(command0.c_str());
+
     string command1 = "g++ " + str + ".cpp -o " + str + ".out";
     string command2 = "./" + str + ".out ";
     system(command1.c_str());
