8c5fa775d287c931922a792c4f2b0e65cfe117af
adityjha0 <aditya2005aps@gmail.com>
2024-02-08 17:57:23 +0530

    main.cpp

--- Stratergies/main.cpp
+++ Stratergies/main.cpp
@@ -4,72 +4,99 @@ using namespace std;
 
 int main(int argv, char *argc[])
 {
-    string cc = "g++ ";
+
     string strategy = argc[1];
     string symbol = argc[2];
     string n = argc[3];
     string x = argc[4];
     string start_date = argc[5];
     string end_date = argc[6];
-    
-    // --------------- generate data --------------------------
-    string command0 = "python3 main.py " + symbol + " " + start_date + " " + end_date;
-    system(command0.c_str());
+    string p = argc[7];
+    string max_hold_days = argc[8];
+    string c1 = argc[9];
+    string c2 = argc[10];
+    string oversold_threshold = argc[11];
+    string overbought_threshold = argc[12];
+    string adx_threshold = argc[13];
+    string train_start_date = argc[14];
+    string train_end_date = argc[15];
+    string symbol1 = argc[16];
+    string symbol2 = argc[17];
+    string threshold = argc[18];
+    string stop_loss_threshold = argc[19];
 
+    string command = "";
+
+    // --------------- generate data --------------------------
+    if (strategy == "PAIRS")
+    {
+        command = "python3 main.py " + symbol1 + " " + start_date + " " + end_date;
+        system(command.c_str());
+        command = "python3 main.py " + symbol2 + " " + start_date + " " + end_date;
+        system(command.c_str());
+    }
+    else
+    {
+        command = "python3 main.py " + symbol + " " + start_date + " " + end_date;
+        system(command.c_str());
+    }
+
+
+    // ---------------- case based commands --------------------
     if (strategy == "BASIC")
     {
         system("g++ basic.cpp -o basic.out");
-        string command = "./basic.out " + symbol +" " + n + " " + x;
+        command = "./basic.out " + symbol + " " + n + " " + x;
         system(command.c_str());
     }
     else if (strategy == "DMA")
     {
         system("g++ dma.cpp -o dma.out");
-        string command = "./dma.out ";
+        command = "./dma.out ";
         system(command.c_str());
     }
     else if (strategy == "DMA++")
     {
         system("g++ dmapp.cpp -o dmapp.out");
-        string command = "./dmapp.out ";
+        command = "./dmapp.out ";
         system(command.c_str());
     }
     else if (strategy == "MACD")
     {
         system("g++ macd.cpp -o macd.out");
-        string command = "./macd.out ";
+        command = "./macd.out ";
         system(command.c_str());
     }
     else if (strategy == "RSI")
     {
         system("g++ rsi.cpp -o rsi.out");
-        string command = "./rsi.out ";
+        command = "./rsi.out ";
         system(command.c_str());
     }
     else if (strategy == "ADX")
     {
         system("g++ adx.cpp -o adx.out");
-        string command = "./adx.out ";
+        command = "./adx.out ";
         system(command.c_str());
     }
     else if (strategy == "LINEAR_REGRESSION")
     {
         system("g++ linregr.cpp -o linregr.out");
-        string command = "./linregr.out ";
+        command = "./linregr.out ";
         system(command.c_str());
     }
     else if (strategy == "BEST_OF_ALL")
     {
         system("g++ bestall.cpp -o bestall.out");
-        string command = "./bestall.out ";
+        command = "./bestall.out ";
         system(command.c_str());
     }
     else if (strategy == "PAIRS")
     {
         system("g++ pairs.cpp -o pairs.out");
-        string command = "./pairs.out ";
-        system(command.c_str());        
+        command = "./pairs.out ";
+        system(command.c_str());
     }
-    
+
     return 0;
 }
