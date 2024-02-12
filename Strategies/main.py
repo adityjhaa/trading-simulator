11f91f50f304e29da33d8fefb228a4450539b360
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-02-12 06:21:54 +0530

    Updated main.py

--- Strategies/main.py
+++ Strategies/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from jugaad_data.nse import stock_df
 
 # ------------------------------------ generation of data frame --------------------------------------------
-def generate_dataframe(symbol, today, lastday):
+def generate_dataframe(symbol, today, lastday, train_data):
     # formatting the date
     to_day = int(today[:2])
     to_month = int(today[3:5])
@@ -15,12 +15,14 @@ def generate_dataframe(symbol, today, lastday):
     la_year = int(lastday[6:10])
     
     df = pd.DataFrame(stock_df(symbol=symbol, from_date=date(to_year, to_month, to_day), to_date=date(la_year, la_month, la_day), series="EQ"))
-    df = df[[ "DATE", "CLOSE", "HIGH", "LOW", "PREV. CLOSE", "VWAP", "NO OF TRADES"]]
+    df = df[[ "DATE", "CLOSE", "HIGH", "LOW", "PREV. CLOSE", "VWAP", "NO OF TRADES", "OPEN"]]
     df = df.iloc[::-1]
     df['DATE'] = pd.to_datetime(df['DATE'], format='%d-%m-%Y')
     df['DATE'] = df['DATE'].dt.strftime('%d/%m/%Y')
-    df.to_csv("Stocks/"+symbol+".csv", index=False)
-
+    if (train_data == "0"):
+        df.to_csv("Stocks/"+symbol+".csv", index=False)
+    else:
+        df.to_csv("Stocks/"+symbol+"_train.csv", index = False)
 
 # ----------------------------------------------- pickle --------------------------------------------------------
 def write_pickle(DATA, symbol):
@@ -31,8 +33,9 @@ def main():
     argument = sys.argv[1]
     today = sys.argv[2]
     lastday = sys.argv[3]
+    train_data = sys.argv[4]
 
-    generate_dataframe(argument, today, lastday)
+    generate_dataframe(argument, today, lastday, train_data)
     #write_pickle(DATA, "Stocks/" + argument)
 
 
